cdb2b2fee04f39e923896d875160b9fcc92c17ef
naga-git <nagarjuna0219@gmail.com>
2023-05-31 15:07:14 +0530

    first commit

---
+++ additiongt/src/com/tcs/git/Addition.java
@@ -0,0 +1,18 @@
+package com.tcs.git;
+
+public class Addition {
+
+	public static void main(String[] args) {
+	
+
+				int num1=10;
+				int num2=20;
+				
+		System.out.println(num1+num2);
+
+	
+
+
+	}
+
+}
